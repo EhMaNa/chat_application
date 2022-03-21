dc298a4cce0a6762576f317dc4ba592c3a85e8ad
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-21 20:20:56 +0000

    Update chatlist_screen.dart

---
+++ lib/chatlist_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  late TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 2, vsync: this, initialIndex: 0);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CHaT'),
+        actions: [
+          IconButton(icon: Icon(Icons.search_sharp), onPressed: (){},),
+          IconButton(icon: Icon(Icons.more_vert_sharp), onPressed: (){},),
+        ],
+        bottom: TabBar(controller: _controller,
+          tabs: [
+            Tab(text: 'Chats',),
+            Tab(text: 'Calls')
+          ],),
+      ),
+      body: TabBarView(controller: _controller,
+        children: [
+          Center(child: Text('Chats'),),
+          Center(child: Text('Calls'),),
+        ],),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: Icon(Icons.message_sharp),
+      ),
+    );
+  }
+}
