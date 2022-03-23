4007d50e0d57a063ec5abdf28f8b98b1a4e25bab
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-23 19:45:52 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Chat Application',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: "OpenSans"
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
