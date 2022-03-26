670e5b2b728255f60e4462f79ca4b439024044d1
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-26 13:27:26 +0000

    Update card.dart

--- lib/styles/card.dart
+++ lib/styles/card.dart
@@ -10,24 +10,34 @@ class MyCard extends StatefulWidget {
 class _MyCardState extends State<MyCard> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-      ),
-      title: Text('Checking', style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 16,
-      ),),
-      subtitle: Row(
-        children: [
-          Icon(Icons.done_all),
-          Text('fa wo gyimie ko', style: TextStyle(
-            fontSize: 13,
+    return Column(
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+          ),
+          title: Text('Checking', style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
           ),),
-          SizedBox(width: 3,)
-        ],
-      ),
-      trailing: Text('20:48'),
+          subtitle: Row(
+            children: [
+              Icon(Icons.done_all),
+              Text('fa wo gyimie ko', style: TextStyle(
+                fontSize: 13,
+              ),),
+              SizedBox(width: 3,)
+            ],
+          ),
+          trailing: Text('20:48'),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20, left: 80),
+          child: Divider(
+            thickness: 0.5,
+          ),
+        )
+      ],
     );
   }
 }
