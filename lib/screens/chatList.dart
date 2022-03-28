dd238d5ce51d57b7c6c9c90f25ee5dc3e4f694be
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-28 13:55:30 +0000

    Update chatList.dart

--- lib/screens/chatList.dart
+++ lib/screens/chatList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:folder/models/chat.dart';
 import 'package:folder/styles/card.dart';
 
 class ChatList extends StatefulWidget {
@@ -11,14 +12,19 @@ class ChatList extends StatefulWidget {
 //This Screen Shows the List of Chats
 
 class _ChatListState extends State<ChatList> {
+  List<ChatModel> chats = [
+    ChatModel(name: "Tron",time: '12:30', icon: 'person.svg', message: "Arivadache"),
+  ChatModel(name: "Psam",time: '12:30', icon: 'person.svg', message: "Hola");
+  ]
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          MyCard(),
-          MyCard()
-        ],
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index) => MyCard()
+
+        },
+
       ),
     );
   }
