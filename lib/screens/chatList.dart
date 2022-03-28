7e2413ff28815b814df376b0ae77bb3b28a71a55
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-28 16:02:10 +0000

    Update chatList.dart

--- lib/screens/chatList.dart
+++ lib/screens/chatList.dart
@@ -14,7 +14,7 @@ class ChatList extends StatefulWidget {
 class _ChatListState extends State<ChatList> {
   List<ChatModel> chats = [
     ChatModel(name: "Tron",time: '12:30 AM', icon: 'person.svg', message: "Arivadache", inGroup: false),
-  ChatModel(name: "Psam",time: '1:39 PM', icon: 'person.svg', message: "Hola", inGroup: false),
+  ChatModel(name: "Psam",time: '1:39 PM', icon: 'person.svg', message: "Hola", inGroup: true),
   ];
   @override
   Widget build(BuildContext context) {
