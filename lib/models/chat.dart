ce894a9b86ab6c99419c7813f7a1bd828ebcd900
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-03-31 23:56:20 +0000

    removed file

--- lib/models/chat.dart
+++
@@ -1,9 +0,0 @@
-
-class ChatModel {
-  String name;
-  String time;
-  String? icon;
-  bool inGroup;
-  String message;
-  ChatModel({required this.name, this.icon, required this.time, required this.message, required this.inGroup});
-}
