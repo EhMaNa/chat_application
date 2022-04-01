5952f5e51a56fa4db3efaa7fa1dd8e004fdb799e
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-04-01 14:42:10 +0000

    Update chatPage.dart

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:folder/models/chatModel.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChatPage extends StatefulWidget {
-  /*const ChatPage(this.chatModel);
-  final ChatModel chatModel;*/
+  const ChatPage(this.chatModel);
+  final ChatModel chatModel;
 
   @override
   _ChatPageState createState() => _ChatPageState();
@@ -21,6 +22,11 @@ class _ChatPageState extends State<ChatPage> {
             SizedBox(width: 5,),
             CircleAvatar(
               radius: 20,
+              child: SvgPicture.asset(widget.chatModel.inGroup ? 'svg/person_black_36dp.svg' : 'svg/group_black_36dp.svg',
+                color: Colors.white,
+                height: 30,
+                width: 30,
+              )
             ),
           ],
         ),
