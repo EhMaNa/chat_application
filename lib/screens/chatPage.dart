426b9679b69fe8037cc9e4186d5b742f51f75e11
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-04-01 14:14:12 +0000

    Update chatPage.dart

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:folder/models/chatModel.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage(this.chatModel);
-  final ChatModel chatModel;
+  /*const ChatPage(this.chatModel);
+  final ChatModel chatModel;*/
 
   @override
   _ChatPageState createState() => _ChatPageState();
@@ -14,7 +14,16 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        leadingWidth: 70,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 5,),
+            CircleAvatar(
+              radius: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
