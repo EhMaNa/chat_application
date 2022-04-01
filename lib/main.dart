52f9b1c29765beee187b71c77cb7470f92cfbebc
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-04-01 14:42:16 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:folder/screens/chatPage.dart';
 import 'screens/home_screen.dart';
-import 'package:folder/models/chatModel.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of the application.
-  //late final ChatModel chatModel;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => Home(),
-        '/chat' : (context) => ChatPage(/*chatModel*/)
+        //'/chat' : (context) => ChatPage(chatModel)
       },
     );
   }
