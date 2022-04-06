de9453148500bf5e702ba2a8035229f7939c75ac
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-04-06 22:09:20 +0000

    Update chatPage.dart

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage(this.chatModel);
+
   final ChatModel chatModel;
 
   @override
@@ -19,41 +20,62 @@ class _ChatPageState extends State<ChatPage> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(width: 5,),
-            CircleAvatar(
-              radius: 20,
-              child: SvgPicture.asset(widget.chatModel.inGroup ? 'svg/person_black_36dp.svg' : 'svg/group_black_36dp.svg',
-                color: Colors.white,
-                height: 30,
-                width: 30,
-              )
+            SizedBox(
+              width: 5,
             ),
-            SizedBox(width: 10,),
+            CircleAvatar(
+                radius: 20,
+                child: SvgPicture.asset(
+                  widget.chatModel.inGroup
+                      ? 'svg/person_black_36dp.svg'
+                      : 'svg/group_black_36dp.svg',
+                  color: Colors.white,
+                  height: 30,
+                  width: 30,
+                )),
+            SizedBox(
+              width: 10,
+            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(widget.chatModel.name, style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.5,
-                ),),
-                Text("Online",style: TextStyle(
-                  fontSize: 13,
-                  fontWeight: FontWeight.w100,
-                ),)
+                Text(
+                  widget.chatModel.name,
+                  style: TextStyle(),
+                ),
+                Text(
+                  "online",
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w100,
+                  ),
+                )
               ],
             )
           ],
         ),
         actions: [
-          IconButton(icon: Icon(Icons.call), onPressed: (){},),
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () {},
+          ),
           PopupMenuButton(
-              onSelected: (value){},
+              onSelected: (value) {},
               itemBuilder: (buildContext) {
                 return [
-                  PopupMenuItem(child: Text('New group'), value: 'New group',),
-                  PopupMenuItem(child: Text('Broadcast'), value: 'Broadcast',),
-                  PopupMenuItem(child: Text('Settings'), value: 'Settings',)
+                  PopupMenuItem(
+                    child: Text('New group'),
+                    value: 'New group',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Broadcast'),
+                    value: 'Broadcast',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Settings'),
+                    value: 'Settings',
+                  )
                 ];
               })
         ],
