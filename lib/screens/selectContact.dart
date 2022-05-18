05105d4b77de11c80f64698b89d5cc16eaa4f78d
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-05-18 13:53:08 +0000

    creating contact page

---
+++ lib/screens/selectContact.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
