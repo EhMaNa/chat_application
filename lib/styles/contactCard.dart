ff63776d131efae08008e7d9cd6ff07ce3b70d58
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-05-18 14:31:30 +0000

    contact card

---
+++ lib/styles/contactCard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ContactCard extends StatelessWidget {
+  const ContactCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 23,
+        child: SvgPicture.asset('svg/group_black_36dp.svg'),
+      ),
+    );
+  }
+}
