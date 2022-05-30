e8bbb1b1ba32364753f33d792c658f487a5d6af6
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-05-30 00:37:42 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => Home(),
-        //'/contact' : (context) => ChatPage()
+        //'/contact' : (context) => ContactPage()
       },
     );
   }
