7052b05f85da7d9eb4088900bb06c77e8630f647
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-05-31 19:34:16 +0000

    Update chatList.dart

--- lib/screens/chatList.dart
+++ lib/screens/chatList.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:folder/models/chatModel.dart';
 import 'package:folder/styles/card.dart';
 
+
+//This Screen Shows the List of Chats
 class ChatList extends StatefulWidget {
   const ChatList({Key? key}) : super(key: key);
 
   @override
   _ChatListState createState() => _ChatListState();
 }
-
-//This Screen Shows the List of Chats
-
 class _ChatListState extends State<ChatList> {
   List<ChatModel> chats = [
     ChatModel(name: "Tron",time: '12:30 AM', icon: 'person.svg', message: "Arivadache", inGroup: false),
