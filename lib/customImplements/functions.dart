0fee52353a87de850276ec63270f7dab645c993f
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-06-08 21:01:38 +0000

    Update functions.dart

--- lib/customImplements/functions.dart
+++ lib/customImplements/functions.dart
@@ -1,14 +1,19 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+// Contains some custom functions and refactored widgets
+
 Color generateColor (){
-  int random = Random().nextInt(5);
+  int random = Random().nextInt(8);
     Map colors = {
       '0' : Colors.pink,
       '1': Colors.blueAccent,
       '2': Colors.orange,
       '3': Colors.teal,
-      '4': Colors.purple
+      '4': Colors.purple,
+      '5': Colors.brown,
+      '6': Colors.red,
+      '7': Colors.grey,
     };
 
   return colors['$random'];
