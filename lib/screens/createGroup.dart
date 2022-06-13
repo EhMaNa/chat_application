17c96bf3588fdcdd588221dbfa74acbc183db0cb
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-06-13 19:58:23 +0000

    create a group

---
+++ lib/screens/createGroup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewGroup extends StatefulWidget {
+  const NewGroup({Key? key}) : super(key: key);
+
+  @override
+  _NewGroupState createState() => _NewGroupState();
+}
+
+class _NewGroupState extends State<NewGroup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
