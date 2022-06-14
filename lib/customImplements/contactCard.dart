5faaeabad0b7679920e5df401c34d656136c8fd2
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-06-14 19:34:49 +0000

    Update contactCard.dart

--- lib/customImplements/contactCard.dart
+++ lib/customImplements/contactCard.dart
@@ -10,23 +10,20 @@ class ContactCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(3.0, 4.0, 0.0, 4.0),
-      child: InkWell(
-        onTap: (){},
-        child: ListTile(
-          leading: CircleAvatar(
-            radius: 23,
-            child: SvgPicture.asset(chatModel.name == 'New group' ? 'svg/group_black_36dp.svg' : 'svg/person_black_36dp.svg',
-            color: Colors.white,
-            height: 30,
-            width: 30,),
-            backgroundColor: Colors.blueGrey,
-          ),
-          title: Text(chatModel.name,
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 23,
+          child: SvgPicture.asset(chatModel.name == 'New group' ? 'svg/group_black_36dp.svg' : 'svg/person_black_36dp.svg',
+          color: Colors.white,
+          height: 30,
+          width: 30,),
+          backgroundColor: Colors.blueGrey,
         ),
+        title: Text(chatModel.name,
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),),
       ),
     );
   }
