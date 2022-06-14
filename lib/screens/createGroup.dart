434d8f17a82bd913f0098f1c9a0e528b3d7b600f
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-06-14 18:38:19 +0000

    Update createGroup.dart

--- lib/screens/createGroup.dart
+++ lib/screens/createGroup.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:folder/customImplements/contactCard.dart';
+import 'package:folder/customImplements/myImplements.dart';
 
 class NewGroup extends StatefulWidget {
-  const NewGroup({Key? key}) : super(key: key);
+
 
   @override
   _NewGroupState createState() => _NewGroupState();
@@ -10,6 +12,23 @@ class NewGroup extends StatefulWidget {
 class _NewGroupState extends State<NewGroup> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Select Contact',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15
+            ),),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search_sharp),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (context, index) => ContactCard(chats[index]))
+    );
   }
 }
