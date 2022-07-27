d068c8be69d9a9293dca07a2c6abaf5f4c74bee9
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-07-27 13:18:01 +0000

    moved project to this folder

---
+++ chat_application/lib/screens/selectContact.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:folder/customImplements/contactCard.dart';
+import 'package:folder/customImplements/myImplements.dart';
+import 'package:folder/screens/chatPage.dart';
+import 'package:folder/screens/createGroup.dart';
+
+// Show Contacts Screen
+class ContactsPage extends StatefulWidget {
+  const ContactsPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactsPageState createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends State<ContactsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Contact',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16
+        ),),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search_sharp),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: chats.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: (){
+                    index != 0
+                        ? Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ChatPage(chats[index])))
+                        : Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => NewGroup()));
+                  },
+                child: ContactCard(chats[index], 'Contact'));
+          })
+    );
+  }
+}
