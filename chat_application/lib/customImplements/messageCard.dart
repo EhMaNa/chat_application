978683fa5261cdeb4a3cfdf5e973734aa7e6c669
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-08-03 12:52:30 +0000

    Create messageCard.dart

---
+++ chat_application/lib/customImplements/messageCard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SendCard extends StatefulWidget {
+  const SendCard({Key? key}) : super(key: key);
+
+  @override
+  _SendCardState createState() => _SendCardState();
+}
+
+class _SendCardState extends State<SendCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
