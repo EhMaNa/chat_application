ff0f28a6e911abc2bfe80b4a22ec8866b7cd9bd5
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-08-04 12:53:33 +0000

    building message bubbles for sender

---
+++ chat_application/lib/customImplements/messageBubbles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SendBubble extends StatefulWidget {
+  const SendBubble({Key? key}) : super(key: key);
+
+  @override
+  _SendBubbleState createState() => _SendBubbleState();
+}
+
+class _SendBubbleState extends State<SendBubble> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Material(
+        elevation: 5,
+        color: Colors.lightBlueAccent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
+          child: Text('Hello',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15.0
+          ),),
+        ),
+      ),
+    );
+
+
+
+      /*Align(
+      alignment: Alignment.centerRight,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width - 45,
+        ),
+        child: Card(
+            child: Stack(
+              children: [
+                Text('Hello'),
+                Row(
+                  children: [
+                    Text('12:30 AM'),
+                    Icon(Icons.done_all)
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );*/
+  }
+}
