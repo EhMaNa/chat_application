a8cdde341356461744ddd5e053a513f8795fb514
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-02-06 20:13:48 +0000

    Update main.dart

--- chat_application/lib/main.dart
+++ chat_application/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,18 +13,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CHaT',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: "OpenSans"
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "OpenSans"),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => Home(),
+        '/': (context) => MyWidget(),
+        '/home': (context) => Home(),
         //'/group' : (context) => ContactPage()
       },
     );
   }
 }
-
-
