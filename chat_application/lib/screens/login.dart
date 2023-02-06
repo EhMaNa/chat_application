70a6b5af5323786d43ee1160d581e074d55035ed
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-02-06 20:13:38 +0000

    Update login.dart

--- chat_application/lib/screens/login.dart
+++ chat_application/lib/screens/login.dart
@@ -13,38 +13,42 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Column(
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: ListTile(
-              leading: Icon(
-                Icons.person,
-                size: 25,
-              ),
-              title: Text(
-                chats[1].name,
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: ListTile(
-              leading: Icon(
-                Icons.person,
-                size: 25,
-              ),
-              title: Text(
-                chats[1].name,
-                style: TextStyle(fontSize: 20),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: ListTile(
+                leading: Icon(
+                  Icons.person,
+                  size: 25,
+                ),
+                title: Text(
+                  chats[1].name,
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.popAndPushNamed(context, '/home'),
+              child: ListTile(
+                leading: Icon(
+                  Icons.person,
+                  size: 25,
+                ),
+                title: Text(
+                  chats[2].name,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
