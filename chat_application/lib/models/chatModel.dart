3621167b3406970edac186a1239b5cd882fae568
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-02-17 19:19:44 +0000

    update: add id's to chatModel

--- chat_application/lib/models/chatModel.dart
+++ chat_application/lib/models/chatModel.dart
@@ -1,10 +1,16 @@
-
 class ChatModel {
+  int id;
   String name;
   String time;
   String? icon;
   bool inGroup;
   String message;
   bool select = false;
-  ChatModel({required this.name, this.icon, required this.time, required this.message, required this.inGroup});
+  ChatModel(
+      {required this.id,
+      required this.name,
+      this.icon,
+      required this.time,
+      required this.message,
+      required this.inGroup});
 }
