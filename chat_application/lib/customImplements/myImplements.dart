a26f951d9d2e0672b098cfd289391f5bf1f0e815
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-02-17 19:21:20 +0000

    update: add id's to chatModel

--- chat_application/lib/customImplements/myImplements.dart
+++ chat_application/lib/customImplements/myImplements.dart
@@ -4,24 +4,36 @@ import 'package:folder/models/chatModel.dart';
 
 // Contains some custom functions and refactored widgets
 
-Color generateColor (){
+Color generateColor() {
   int random = Random().nextInt(8);
-    Map colors = {
-      '0' : Colors.pink,
-      '1': Colors.blueAccent,
-      '2': Colors.orange,
-      '3': Colors.teal,
-      '4': Colors.purple,
-      '5': Colors.brown,
-      '6': Colors.red,
-      '7': Colors.grey,
-    };
+  Map colors = {
+    '0': Colors.pink,
+    '1': Colors.blueAccent,
+    '2': Colors.orange,
+    '3': Colors.teal,
+    '4': Colors.purple,
+    '5': Colors.brown,
+    '6': Colors.red,
+    '7': Colors.grey,
+  };
 
   return colors['$random'];
 }
 
 List<ChatModel> chats = [
-  ChatModel(name: "New group",time: '', message: "", inGroup: true),
-  ChatModel(name: "Tron",time: '12:30 AM', icon: 'person.svg', message: "Arivadache", inGroup: true),
-  ChatModel(name: "Psam",time: '1:39 PM', icon: 'person.svg', message: "Hola", inGroup: true),
+  ChatModel(id: 0, name: "New group", time: '', message: "", inGroup: true),
+  ChatModel(
+      id: 1,
+      name: "Tron",
+      time: '12:30 AM',
+      icon: 'person.svg',
+      message: "Arivadache",
+      inGroup: true),
+  ChatModel(
+      id: 2,
+      name: "Psam",
+      time: '1:39 PM',
+      icon: 'person.svg',
+      message: "Hola",
+      inGroup: true),
 ];
