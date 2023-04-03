61adbfeff75eb6f7e6ada06e6c5fcd330800e65d
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2023-04-03 12:28:22 +0000

    fix: correctly point to assets folder

--- chat_application/lib/customImplements/card.dart
+++ chat_application/lib/customImplements/card.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:folder/models/chatModel.dart';
 import 'package:folder/screens/chatPage.dart';
 
-
 class MyCard extends StatefulWidget {
   final ChatModel chatModel;
   const MyCard(this.chatModel);
@@ -16,33 +15,49 @@ class _MyCardState extends State<MyCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(widget.chatModel)));
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ChatPage(widget.chatModel)));
       },
       child: Column(
         children: [
           ListTile(
             leading: CircleAvatar(
               radius: 30,
-              child: SvgPicture.asset(widget.chatModel.inGroup ? 'svg/person_black_36dp.svg' : 'svg/group_black_36dp.svg',
+              child: SvgPicture.asset(
+                widget.chatModel.inGroup
+                    ? 'assets/svg/person_black_36dp.svg'
+                    : 'assets/svg/group_black_36dp.svg',
                 color: Colors.white,
                 height: 30,
                 width: 30,
               ),
               backgroundColor: Colors.blueGrey,
             ),
-            title: Text(widget.chatModel.name, style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),),
+            title: Text(
+              widget.chatModel.name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
             subtitle: Row(
               children: [
                 Icon(Icons.done_all),
-                SizedBox(width: 2,),
-                Text(widget.chatModel.message, style: TextStyle(
-                  fontSize: 13,
-                ),),
-                SizedBox(width: 3,)
+                SizedBox(
+                  width: 2,
+                ),
+                Text(
+                  widget.chatModel.message,
+                  style: TextStyle(
+                    fontSize: 13,
+                  ),
+                ),
+                SizedBox(
+                  width: 3,
+                )
               ],
             ),
             trailing: Text(widget.chatModel.time),
